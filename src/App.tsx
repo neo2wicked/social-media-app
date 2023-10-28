d2265cf4d7913eb4f68a1099b9a33d412ece3515
neo2wicked <capricorn@Live.com>
2023-10-28 08:55:51 -0500

    added public and private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './globals.css';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello, Life Online!
-    </h1>
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            <Route path="/sign-in" element={<SignInForm />} />
+            
+            {/* private routes */}
+            <Route index element={<Home />}/>
+        </Routes>
+        </main>
   )
 }
 
