d438c6b0b305d17383c6d5c879b222da48272c8d
neo2wicked <capricorn@Live.com>
2023-10-29 17:20:30 -0500

    updated application with appwrite functionalities

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,7 @@ import { useForm } from "react-hook-form"
 import { SignupValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader";
 import { Link } from 'react-router-dom';
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -32,7 +33,7 @@ const SignupForm = () => {
    
     // 2. Define a submit handler.
     async function onSubmit(values: z.infer<typeof SignupValidation>) {
-      // const newUser = await createUserAccount(values);
+      const newUser = await createUserAccount(values);
     }
   return (
     
